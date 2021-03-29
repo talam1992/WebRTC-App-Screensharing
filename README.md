710fcbb329df9263e425156c58804e3f27a4ed1c
tal1992 <timothylam92@hotmail.co.uk>
2021-03-29 18:46:11 +0100

    Screensharing

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# WebRTC App Screensharing
- Screensharing
+# WebRTC App
+WebRTC 
+
+https://www.textfixer.com/html/compress-html-compression.php
+
+ Installation
+============================================================
+
+```
+https://nodejs.org/en/
+```
+
+```
+npm init
+```
+
+```
+npm install wscat
+```
+
+```
+npm install -g node static
+```
+
+Running WebRTC-App
+============================================================
+```
+static
+```
+
+```
+node server
+```
+
+```
+http://localhost:8080/login
+```
---
+++ server.js
@@ -0,0 +1,126 @@
+var webSocketServ = require('ws').Server;
+
+var wss = new webSocketServ({
+    port:9090
+});
+
+var users = {};
+var otherUser;
+wss.on('connection', function(conn) {
+    console.log("User connected");
+
+    conn.on('message', function(message){
+        var data;
+
+        try{
+            data = JSON.parse(message);
+        } catch (e) {
+            console.log("Invalid JSON");
+            data = {};
+        }
+
+        switch(data.type){
+            case "login":
+                if(users[data.name]) {
+                    sendToOtherUser(conn, {
+                        type: "login",
+                        success: false
+                    })
+                } else {
+                    users[data.name] = conn;
+                    conn.name = data.name
+                    sendToOtherUser(conn, {
+                        type: "login",
+                        success: true
+                    })
+                }
+                break;
+            case "offer":
+                var connect = users[data.name];
+                if (connect != null) {
+                    conn.otherUser = data.name;
+
+                    sendToOtherUser(connect, {
+                        type: "offer",
+                        offer: data.offer,
+                        name: conn.name
+                    })
+                }
+                break;
+            case "answer":
+                var connect = users[data.name];
+                if (connect != null) {
+                    conn.otherUser = data.name
+                    sendToOtherUser(connect, {
+                        type: "answer",
+                        answer: data.answer
+                    })
+                }
+                break;
+            case "candidate":
+                var connect = users[data.name];
+                if (connect != null) {
+                    sendToOtherUser(connect, {
+                        type: "candidate",
+                        candidate: data.candidate
+                    })
+                }
+                break;
+            case "reject":
+                var connect = users[data.name];
+                if (connect != null) {
+                    sendToOtherUser(connect, {
+                        type: "reject",
+                        name: conn.name
+                    })
+                }
+                break;
+            case "accept":
+                var connect = users[data.name];
+                if (connect != null) {
+                    sendToOtherUser(connect, {
+                        type: "accept",
+                        name: conn.name
+                    })
+                }
+                break;
+            case "leave":
+                var connect = users[data.name];
+                connect.otherUser = null;    
+                if (connect != null) {
+                    sendToOtherUser(connect, {
+                        type: "leave"
+                    })
+                }
+                break;
+            default:
+                sendToOtherUser(conn, {
+                    type: "error",
+                    message: "Command not found: " + data.type
+                });
+                break;
+        }
+    })
+    conn.on('close', function(message){
+        console.log('Connection closed');
+        if(conn.name){
+            delete users[conn.name];
+            if(conn.otherUser){
+                var connect = users[conn.otherUser];
+                conn.otherUser = null;
+
+                if(conn != null){
+                    sendToOtherUser(connect, {
+                        type:"leave"
+                    } )
+                }
+            }
+        }
+    })
+
+    conn.send("Hello World");
+})
+
+function sendToOtherUser (connection, message) {
+    connection.send(JSON.stringify(message))
+}
